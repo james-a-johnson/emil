18a4f13a1a91a288b8ce3a5dbdbf67ffe5696118
James Johnson <jjohnsonjj1251@gmail.com>
2025-09-13 12:08:53 -0400

    Add hooks to implement instructions that binja can't lift

--- src/bin/read-stdin.rs
+++ src/bin/read-stdin.rs
@@ -8,7 +8,8 @@ use binaryninja::binary_view::{BinaryViewBase, BinaryViewExt};
 use binaryninja::headless::Session;
 
 use emil::arch::{riscv::*, State, SyscallResult};
-use emil::emulate::{Emulator, Exit, HookStatus, Little};
+use emil::emil::ILVal;
+use emil::emulate::{Emulator, Endian, Exit, HookStatus, Little};
 use emil::prog::Program;
 use emil::os::linux::{AuxVal, Environment, LinuxSyscalls};
 use softmew::page::{Page, SimplePage};
@@ -177,7 +178,8 @@ impl LinuxSyscalls<Rv64State, MMU<SimplePage>> for RvMachine {
 fn main() {
     let required_functions: &[u64] = &[
         0x1054c, 0x1056e, 0x1073a, 0x29294, 0x29ce8, 0x47830, 0x47824, 0x28ae6, 0x10a90, 0x28962,
-        0x281d2, 0x28fcc, 0x47848, 0x4783c, 0x26c18, 0x26bec, 0x24dfe, 0x24e92, 0x28988,
+        0x281d2, 0x28fcc, 0x47848, 0x4783c, 0x26c18, 0x26bec, 0x24dfe, 0x24e92, 0x28988, 0x14b5c,
+        0x14aa2, 0x223ba,
     ];
     let headless_session = Session::new().expect("Failed to create new session");
     let bv = headless_session
@@ -251,6 +253,8 @@ fn main() {
     state.regs_mut()[Rv64Reg::sp] = sp_val;
 
     let mut emu = Emulator::new(prog, state);
+    emu.add_hook(0x14ac6, store_conditional_hook).expect("Failed to install first hook");
+    emu.add_hook(0x223dc, store_conditional_hook2).expect("Failed to install second hook");
     // emu.add_hook(0x29318, compare_hook);
     // This is the it just returns without parsing the auxiliary vectors
     // emu.add_breakpoint(0x294ca);
@@ -272,10 +276,10 @@ fn main() {
                 }
             }
         } else {
+            println!("Wat");
             break;
         }
     }
-    let stop_reason = emu.run(bv.entry_point());
     println!("Stop reason: {:?}", stop_reason);
     println!("Stopped at: {:#x}", emu.curr_pc());
 
@@ -292,4 +296,24 @@ fn compare_hook(state: &mut dyn State<Reg = Rv64Reg, Endianness = Little>) -> Ho
     } else {
         HookStatus::Continue
     }
+}
+
+fn store_conditional_hook(state: &mut dyn State<Reg = Rv64Reg, Endianness = Little>) -> HookStatus {
+    let mut buf = [0u8; 8];
+    let addr = state.read_reg(Rv64Reg::a1).extend_64();
+    let value = state.read_reg(Rv64Reg::a4).truncate(4);
+    Little::write(value, &mut buf);
+    let _ = state.write_mem(addr, &buf[0..4]);
+    state.write_reg(Rv64Reg::a2, ILVal::Word(0));
+    HookStatus::Goto(0x14aca)
+}
+
+fn store_conditional_hook2(state: &mut dyn State<Reg = Rv64Reg, Endianness = Little>) -> HookStatus {
+    let mut buf = [0u8; 8];
+    let addr = state.read_reg(Rv64Reg::s0).extend_64();
+    let value = state.read_reg(Rv64Reg::a4).truncate(4);
+    Little::write(value, &mut buf);
+    let _ = state.write_mem(addr, &buf[0..4]);
+    state.write_reg(Rv64Reg::a3, ILVal::Word(0));
+    HookStatus::Goto(0x223e0)
 }
