f2b1a9d2dc6f361806a2d07d61f06c37af96ebfb
James Johnson <jjohnsonjj1251@gmail.com>
2025-11-14 19:41:43 -0500

    Improve generic register implementation

--- emulator/src/arch/generic.rs
+++ emulator/src/arch/generic.rs
@@ -1,13 +1,14 @@
-use crate::arch::{Endian, RegState};
+use crate::arch::{Big, Endian, Little, RegState};
 use crate::emil::ILVal;
+use std::collections::HashMap;
 use std::rc::Rc;
-use std::{collections::HashMap, marker::PhantomData};
 
 use std::ops::{Index, IndexMut};
 
+use binaryninja::Endianness;
 use binaryninja::architecture::{Architecture, CoreArchitecture, Register, RegisterInfo};
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct RegId(u32);
 
 impl std::fmt::Display for RegId {
@@ -35,7 +36,7 @@ struct RegInfo {
     size: usize,
 }
 
-pub struct GenericRegs<E> {
+pub struct GenericRegs {
     /// All of the registers as a flat array of bytes.
     ///
     /// Register accesses will just access some number of contiguous bytes in this array.
@@ -44,13 +45,14 @@ pub struct GenericRegs<E> {
     regs: HashMap<u32, Rc<RegInfo>>,
     /// Mapping of register name to offset and number of bytes in the `data` field.
     reg_name: HashMap<String, Rc<RegInfo>>,
-    /// Register ID to use as the syscall return register.
-    syscall_id: u32,
-    _e: PhantomData<E>,
+    /// Function to read bytes in either little or bit endian format.
+    read_endian: fn(data: &[u8]) -> ILVal,
+    /// Function to write data with the correct endianness.
+    write_endian: fn(value: ILVal, data: &mut [u8]),
 }
 
-impl<E: Endian> GenericRegs<E> {
-    pub fn parse_from_arch(arch: &CoreArchitecture, sys_ret_id: u32) -> Self {
+impl GenericRegs {
+    pub fn parse_from_arch(arch: &CoreArchitecture) -> Self {
         let mut data = Vec::new();
         let mut map = HashMap::new();
         let mut names = HashMap::new();
@@ -88,58 +90,65 @@ impl<E: Endian> GenericRegs<E> {
             names.insert(reg.name().into_owned(), info);
         }
 
-        Self {
-            data,
-            regs: map,
-            syscall_id: sys_ret_id,
-            reg_name: names,
-            _e: PhantomData,
+        if arch.endianness() == Endianness::LittleEndian {
+            Self {
+                data,
+                regs: map,
+                reg_name: names,
+                read_endian: Little::read,
+                write_endian: Little::write,
+            }
+        } else {
+            Self {
+                data,
+                regs: map,
+                reg_name: names,
+                read_endian: Big::read,
+                write_endian: Big::write,
+            }
         }
     }
 
-    pub fn read_reg(&self, id: RegId) -> ILVal {
-        let info = self.regs.get(&id.0).expect("Invalid register id");
-        let offset = info.offset;
-        let size = info.size;
-        match size {
-            1 => ILVal::Byte(self.data[offset]),
-            _ => E::read(&self.data[offset..][..size]),
-        }
-    }
-
-    pub fn write_reg(&mut self, id: RegId, val: ILVal) {
-        let info = self.regs.get(&id.0).expect("Invalid register id");
-        let offset = info.offset;
-        let size = info.size;
-        E::write(val, &mut self.data[offset..][..size]);
+    pub fn reg_names(&self) -> impl Iterator<Item = &String> {
+        self.reg_name.keys()
     }
 }
 
-impl<E> Index<&str> for GenericRegs<E> {
+impl From<&CoreArchitecture> for GenericRegs {
+    fn from(value: &CoreArchitecture) -> Self {
+        Self::parse_from_arch(value)
+    }
+}
+
+impl Index<&str> for GenericRegs {
     type Output = [u8];
 
-    /// Get the bytes of a specific register in native endianness.
+    /// Get the bytes of a specific register in host endian format.
     fn index(&self, index: &str) -> &Self::Output {
         let info = self.reg_name.get(index).expect("Invalid register name");
         &self.data[info.offset..][..info.size]
     }
 }
 
-impl<E> IndexMut<&str> for GenericRegs<E> {
+impl IndexMut<&str> for GenericRegs {
     fn index_mut(&mut self, index: &str) -> &mut Self::Output {
         let info = self.reg_name.get(index).expect("Invalid register name");
         &mut self.data[info.offset..][..info.size]
     }
 }
 
-impl<E: Endian> RegState for GenericRegs<E> {
+impl RegState for GenericRegs {
     type RegID = RegId;
 
     fn read(&self, id: Self::RegID) -> ILVal {
-        todo!();
+        let reg_info = self.regs.get(&id.0).expect("Invalid register id");
+        let data = &self.data[reg_info.offset..][..reg_info.size];
+        (self.read_endian)(data)
     }
 
     fn write(&mut self, id: Self::RegID, val: ILVal) {
-        todo!();
+        let reg_info = self.regs.get(&id.0).expect("Invalid register id");
+        let data = &mut self.data[reg_info.offset..][..reg_info.size];
+        (self.write_endian)(val, data);
     }
 }
