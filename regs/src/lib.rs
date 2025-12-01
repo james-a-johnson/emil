627f9650f0402751f08e2ad4582f9ed80594018b
James Johnson <jjohnsonjj1251@gmail.com>
2025-11-30 22:36:30 -0500

    Generate registers for a couple architectures

--- regs/src/lib.rs
+++ regs/src/lib.rs
@@ -1,6 +1,11 @@
 use std::fmt::{Debug, Display};
 use val::ILVal;
 
+pub mod aarch64;
+pub mod rv64gc;
+pub mod x86;
+pub mod x86_64;
+
 const _: () = assert!(
     0xaabb_u16.to_le_bytes()[0] == 0xbb,
     "This crate only supports little endian hosts"
@@ -33,4 +38,39 @@ pub trait RegState {
     /// write so if the value has the wrong size then the program is invalid
     /// and it makes sense to panic.
     fn write(&mut self, id: Self::RegID, val: &ILVal);
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::x86::{x86Reg as X86, x86RegFile as X86State};
+    use val::Big;
+
+    #[test]
+    fn x86_basics() {
+        let mut regs = X86State::default();
+        assert_eq!(regs.eax, 0, "Default values are zero");
+        let eax = regs.read(X86::eax);
+        assert_eq!(eax, ILVal::Word(0), "Reading eax returns a word");
+        regs.write(X86::eax, &ILVal::Word(100));
+        assert_eq!(regs.eax, 100, "Writing eax should change its value");
+        let eax = regs.read(X86::eax);
+        assert_eq!(eax, ILVal::Word(100), "Reading eax returns updated value");
+
+        let st6 = regs.read(X86::st6);
+        assert_eq!(
+            st6,
+            ILVal::Big(Big::from([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])),
+            "Array register should return proper big value"
+        );
+        regs.write(
+            X86::mm6,
+            &ILVal::Quad(u64::from_le_bytes([1, 2, 3, 4, 5, 6, 7, 8])),
+        );
+        assert_eq!(
+            regs.st6,
+            [1, 2, 3, 4, 5, 6, 7, 8, 0, 0],
+            "Setting array register sets proper bytes"
+        );
+    }
 }
